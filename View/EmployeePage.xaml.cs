808d703722aef31c2a699c834e5abc7eb61dbd3e
User <User@B20>
2024-08-28 13:30:09 +0800

    Add project files.

---
+++ View/EmployeePage.xaml.cs
@@ -0,0 +1,12 @@
+namespace Module02Exercise01.View;
+
+using Module02Exercise01.ViewModel;
+public partial class EmployeePage : ContentPage
+{
+	public EmployeePage()
+	{
+		InitializeComponent();
+
+        BindingContext = new EmployeeViewModel();
+    }
+}
